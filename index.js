92a88461511aec00326b6591e34b6776d22b6b0f
joemphilips <joemphilips@gmail.com>
2017-06-24 22:32:36 +0900

    stop using bcoin

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Botkit = require('botkit');
+onst Botkit = require('botkit');
 const config = require('./config');
 const mysql = require('mysql');
 const mongoStorage = require('botkit-storage-mongo')({
@@ -6,46 +6,12 @@ const mongoStorage = require('botkit-storage-mongo')({
 });
 
 // bitcoin
-const bcoin = require('bcoin').set('testnet');
-
-const chain = new bcoin.chain({
-  db: 'leveldb',
-  location: process.env.HOME + '/spvchain',
-  spv: true
+const Client = require('bitcoin-core');
+const client = new Client({
+  network: "regtest",
+  user: 'slackbot',
+  pass: 'bitcoin-tipper'
 });
-const pool = new bcoin.pool({
-  chain: chain,
-  spv: true,
-  maxPeers: 8
-});
-const walletdb = new bcoin.walletdb({
-  db: 'memory'
-});
-
-pool.open().then(function() {
-  return walletdb.open();
-}).then(function() {
-  return walletdb.create();
-}).then(function(wallet) {
-  console.log('created wallet with address %s', wallet.getAddress('base58'));
-
-  // アドレスを監視対象に追加
-  pool.watchAddress(wallet.getAddress());
-
-  // P2Pネットワークからのtxのやり取りを開始
-  pool.connect().then(function() {
-    pool.startSync();
-    pool.on('tx', (tx) => {
-      walletdb.addTx(tx);
-    });
-
-    wallet.on('balance', (balance) => {
-      console.log('balance updated.');
-      console.log(bcoinl.amount.btc(balance.unconfirmed));
-    });
-  });
-});
-
 
 // functions
 
@@ -78,11 +44,27 @@ function saveWallet(userId, passphrase) {
 }
 
 
+// slackbot settings.
+
+let controller = Botkit.slackbot({
+  debug: true
+}).configureSlackApp(
+  config.botconfig
+);
+
+
+controller.spawn({
+  token: process.env.token
+}).startRTM(err => {
+  if (err) {
+    throw new Error(err);
+  }
+});
+
 // help
 
 controller.hears('^help$', ['direct_mention', 'direct_message'], (bot, message) => {
   let usage = `
-  ```
   # show @users bitcoin deposit address
   - @bitcoin-tip depositAddress @user
 
@@ -97,6 +79,5 @@ controller.hears('^help$', ['direct_mention', 'direct_message'], (bot, message) 
 
   # show BTC-JPY rate
   - @bitcoin-tip rate
-  ```
   `;
 });
