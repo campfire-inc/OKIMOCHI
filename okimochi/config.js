745493e49307751c49d44b03e17fcf82ef2aa953
joemphilips <joemphilips@gmail.com>
2017-07-06 18:00:51 +0900

    move all settings to .env file

--- okimochi/config.js
+++ okimochi/config.js
@@ -2,14 +2,14 @@ module.exports = {
   adminPassword: "hoge",
   mongoUri: `mongodb://${process.env.mongo}:27017/example` || `mongodb://${process.env.MONGO_URI}:27017/example` || `mongodb://localhost:27017/example`,
   botconfig: {
-    clientId: "2154447482.200385943586",
-    clientSecret: "9efb47c35019d6e656575e331f5480fc",
+    clientId: process.env.CLIENT_ID ||"2154447482.200385943586",
+    clientSecret: process.env.CLIENT_SECRET || "9efb47c35019d6e656575e331f5480fc",
     scopes: ['bot']
   },
   bitcoin: {
-    network: 'testnet',
-    username: 'slackbot',
-    password: 'bitcoin-tipper',
+    network: process.env.BITCOIND_NETWORK || 'testnet',
+    username: process.env.BITCOIND_USERNAME || 'slackbot',
+    password: process.env.BITCOIND_PASSWORD || 'bitcoin-tipper',
     host: process.env.BITCOIND_URI || "localhost"
   }
 }
