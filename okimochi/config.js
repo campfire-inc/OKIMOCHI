b4d667020b8bb1cd0bbc716dc0e0699bce2a5e18
joemphilips <joemphilips@gmail.com>
2017-07-06 17:54:39 +0900

    stop using external network and links instead

--- okimochi/config.js
+++ okimochi/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   adminPassword: "hoge",
-  mongoUri: `mongodb://${process.env.MONGO_URI}:27017/example` || `mongodb://localhost:27017/example`,
+  mongoUri: `mongodb://${process.env.mongo}:27017/example` || `mongodb://${process.env.MONGO_URI}:27017/example` || `mongodb://localhost:27017/example`,
   botconfig: {
     clientId: "2154447482.200385943586",
     clientSecret: "9efb47c35019d6e656575e331f5480fc",
