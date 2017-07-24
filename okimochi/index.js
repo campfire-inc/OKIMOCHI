4a04e67e658f2ea641769759453898af56322826
joemphilips <joemphilips@gmail.com>
2017-07-24 17:00:47 +0900

    add figure in help message

--- okimochi/index.js
+++ okimochi/index.js
@@ -672,5 +672,15 @@ controller.hears('^rate$', ['direct_mention', 'direct_message'], (bot, message) 
 // help
 controller.hears("help", ["direct_mention", "direct_message"], (bot, message) => {
   bot.reply(message, locale_message.help);
+  const exp_file = "okimochi_explanation.png";
+  bot.api.files.upload({
+    file: fs.createReadStream(path.join(__dirname, "static", "images", exp_file)),
+    filename: exp_file,
+    title: exp_file,
+    channels: message.channel
+  }, (err, res) => {
+    if (err) bot.reply(err)
+    debug("result is ", res);
+  })
 });
 
