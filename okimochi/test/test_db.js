5d83ce84c7594649d45f202c7fe339d14d800946
joemphilips <joemphilips@gmail.com>
2017-08-22 23:23:33 +0900

    run test for help controller

--- okimochi/test/test_db.js
+++ okimochi/test/test_db.js
@@ -1,4 +1,3 @@
 const User = require('../src/db')
 
 
-
---
+++ okimochi/test/test_handler.js
@@ -0,0 +1,31 @@
+const Botmock = require('botkit-mock')
+const helpHandler = require('../src/handlers/help')
+const assert = require('assert')
+const locale_message = require('../config').locale_message
+
+describe('Help Controller Tests', () => {
+  beforeEach(() => {
+    this.controller = Botmock({})
+    this.bot = this.controller.spawn({type: 'slack'})
+    helpHandler(this.controller)
+  })
+
+  it('shout return help message when user asked for help', () => {
+    return this.bot.usersInput(
+      [
+        {
+          user: 'someUserId',
+          channel: 'someChannel',
+          messages: [
+            {
+              text: 'help', isAssertion: true
+            }
+          ]
+        }
+      ]
+    )
+    .then((message) => {
+      return assert.equal(message.text, locale_message.help)
+    })
+  })
+})
