7578b6c289e6b1676e10245041b2d734ac4e4cf3
joemphilips <joemphilips@gmail.com>
2017-08-25 00:10:42 +0900

    remove containers when test done

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 set -ux
 docker network create -d bridge --subnet 182.0.0.0/24 --gateway 182.0.0.1 ci
 docker-compose -f docker-compose.test.yml -p ci build okimochi
-docker-compose -f docker-compose.test.yml -p ci run okimochi
+docker-compose -f docker-compose.test.yml -p ci run --rm okimochi
+docker-compose -f docker-compose.test.yml -p ci down
