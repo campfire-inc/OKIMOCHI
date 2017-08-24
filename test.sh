1b66e0cbc7a4a407c3fbbd9c075af717a9cee344
joemphilips <joemphilips@gmail.com>
2017-08-25 01:11:42 +0900

    test

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@ docker network create -d bridge --subnet 182.0.0.0/24 --gateway 182.0.0.1 ci
 docker-compose -f docker-compose.test.yml -p ci build okimochi
 docker-compose -f docker-compose.test.yml -p ci run --rm okimochi
 docker-compose -f docker-compose.test.yml -p ci down
+
